d86565d5a8c0880c60176a517d264e6b2c752782
Sebastien Ruiz <sruiz@wanadev.fr>
2022-10-26 11:33:45 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,140 @@
+const ws = new WebSocket("ws://localhost:8080");
+
+var msg = document.getElementById('msginput');
+var roomName = document.getElementById('roomName');
+
+var joinedRoom = "";
+
+// var p = null;
+
+var peerList = [];
+var hostPeer = null;
+var isHost = null;
+
+
+function bindEvents(p) {
+
+    p.on('connect', () => {
+        console.log("Connected !");
+        p.send("Connection Established");
+    })
+
+    p.on('data', data => {
+        console.log('data: ' + data)
+        // On renvoit les datas recues a tout les peer
+        if (isHost) {
+            for (let i = 0; i < peerList.length - 1; i++) {
+                peerList[i].send(data);
+            }
+        }
+      })
+
+    p.on('error', function (err) {
+        console.log('error', err);
+    });
+
+    p.on('signal', function (data) {
+        console.log("SIGNAL");
+        //Se triger quand on crée un peer -> JSON.stringify(data) contient notre offer
+        if (isHost) {
+            ws.send(JSON.stringify(
+                {
+                    type: "hostOffer",
+                    roomName: roomName.value,
+                    offer: JSON.stringify(data),
+                }));
+        } else {
+            ws.send(JSON.stringify(
+                {
+                    type: "clientOffer",
+                    roomName: roomName.value,
+                    offer: JSON.stringify(data),
+                }));         
+        }
+    });
+};
+
+function strartPerr (initiator) {
+    console.log("starting peer");
+    let p = new SimplePeer({
+        initiator: initiator,
+        trickle: false,
+    })
+    // for (let i = 0; i < peerList.length; i++) {
+    //     bindEvents(peerList[i]);
+    // }
+    // peerList.push(p);
+    bindEvents(p);
+    return (p);
+};
+
+ws.addEventListener("open", () => {
+    console.log("Connected to server");
+});
+
+ws.addEventListener("message", (e) => {
+    const obj = JSON.parse(e.data);
+
+    if (obj.type === "host" && obj.sucess) {
+        console.log("room " + obj.roomName + " sucessfully created !");
+        joinedRoom = obj.roomName;
+        isHost = true;
+        let p = strartPerr(isHost);
+        peerList.push(p);
+    }
+    else if (obj.type === "join" && obj.sucess) {
+        console.log("room " + obj.roomName + " sucessfully joined !");
+        joinedRoom = obj.roomName;
+        isHost = false;
+        let p = strartPerr(isHost);
+        // peerList.push(p);
+        p.signal(obj.offer);
+        hostPeer = p;
+
+    } else if (obj.type === "recieveClientOffer") {
+        peerList[peerList.length - 1].signal(JSON.parse(obj.offer));
+        // Recréer un peer et envoyer notre offre au server
+        console.log("room " + obj.roomName + " needs host offer update");
+        joinedRoom = obj.roomName;
+        isHost = true;
+        let p = strartPerr(isHost);
+        peerList.push(p);
+
+    }
+});
+
+
+document.querySelector('#createRoom').addEventListener('click', function (e) {
+    ws.send(JSON.stringify(
+        {
+            type: "host",
+            roomName: roomName.value,
+        }));
+})
+
+document.querySelector('#joinRoom').addEventListener('click', function (e) {
+    p = strartPerr(false);
+    ws.send(JSON.stringify(
+        {
+            type: "join",
+            roomName: roomName.value,
+        }));
+})
+
+document.querySelector('#sendMsg').addEventListener('click', function (e) {
+        console.log("Message to send : " + msg.value);
+        if (isHost) {
+            for (let i = 0; i < peerList.length - 1; i++) {
+                console.log("peer to contact : " + peerList[i]);
+                peerList[i].send(msg.value);
+            }
+        } else {
+            if (hostPeer !== null) {
+                hostPeer.send(msg.value);
+            }
+        }
+
+        // if (p !== null) {
+        //     p.send(msg.value);
+        // }
+})
---
+++ server/index.js
@@ -0,0 +1,69 @@
+// launch with : node index.js
+
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 8080 });
+
+var rooms = [];
+var hostOffers = [];
+
+wss.on("connection", ws => {
+    console.log("New connection");
+
+    ws.on("close", () => {
+        console.log("Client disconnected");
+    });
+
+    ws.on("message", (data) => {
+        const obj = JSON.parse(data);
+
+        if (obj.type === "host") {
+            if (!rooms[obj.roomName]) {
+                ws.send(JSON.stringify(
+                    {
+                        type: "host",
+                        sucess: true,
+                        roomName: obj.roomName,
+                    }));
+                rooms[obj.roomName] = ws;
+            }
+            else {
+                ws.send(JSON.stringify(
+                    {
+                        type: "host",
+                        sucess: false,
+                    }));              
+            }
+        }
+        else if (obj.type === "join") {
+            if (rooms[obj.roomName]) {
+                ws.send(JSON.stringify(
+                    {
+                        type: "join",
+                        sucess: true,
+                        roomName: obj.roomName,
+                        offer: hostOffers[obj.roomName],
+                    }));
+            }
+            else {
+                ws.send(JSON.stringify(
+                    {
+                        type: "join",
+                        sucess: false,
+                    }));              
+            }
+            // Stocker l'offre host
+        } else if (obj.type === "hostOffer") {
+            hostOffers[obj.roomName] = obj.offer;
+            console.log("storing " + obj.roomName + " host offer");
+            // Envoyer l'offre client à l'host et inversemenet
+        } else if (obj.type === "clientOffer") {
+            console.log("receiving " + obj.roomName + " client offer");
+            rooms[obj.roomName].send(JSON.stringify(
+                {
+                    type: "recieveClientOffer",
+                    roomName: obj.roomName,
+                    offer: obj.offer,
+                }));
+        }
+    });
+});
